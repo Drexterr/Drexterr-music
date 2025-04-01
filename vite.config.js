b32c14a8b60e957225292e494e572330e933ae41
Drexterr <bharatbanthia2002@gmail.com>
2025-04-01 16:23:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['/Users/Bharat/OneDrive/Desktop/Project/Drexterr Music/Drexterr music/src/img/logo']
+    }
+  }
+
 })
 
